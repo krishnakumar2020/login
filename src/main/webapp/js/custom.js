59929b7fd7e7e2e9be99f582afe08f8aebf2d754
krishna <krish232774@gmail.com>
2020-04-03 04:35:41 -0700

    login 3

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,49 +1,88 @@
 (function ($) {
 
-    "use strict";
+  "use strict";
 
-        // PRE LOADER
-        $(window).load(function(){
-          $('.preloader').delay(500).slideUp('slow'); // set duration in brackets    
-        });
-
-
-        // MENU
-        $('.navbar-collapse a').on('click',function(){
-          $(".navbar-collapse").collapse('hide');
-        });
-
-        $(window).scroll(function() {
-          if ($(".navbar").offset().top > 50) {
-            $(".navbar-fixed-top").addClass("top-nav-collapse");
-              } else {
-                $(".navbar-fixed-top").removeClass("top-nav-collapse");
-              }
-        });
+    // PRE LOADER
+    $(window).load(function(){
+      $('.preloader').fadeOut(1000); // set duration in brackets    
+    });
 
 
-        // PARALLAX JS
-        function initParallax() {
-          $('#home').parallax("60%", 100);
-          $('#about').parallax("100%", 80);
-          $('#project').parallax("80%", 60);
-          $('#team').parallax("40%", 40);
-          $('#contact').parallax("20%", 20);
+    // MENU
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {
+      if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
           }
-        initParallax();
+    });
 
 
-        // Owl Carousel
-        var owl = $("#owl-team");
-          owl.owlCarousel({
-            autoPlay: 6000,
-            items : 4,
-            itemsDesktop : [1199,3],
-            itemsDesktopSmall : [979,3],
-            itemsTablet: [768,2],
-            itemsTabletSmall: false,
-            itemsMobile : [479,1],
-            Speedfast: 200,
-        });
+    // HOME SLIDER & COURSES & CLIENTS
+    $('.home-slider').owlCarousel({
+      animateOut: 'fadeOut',
+      items:1,
+      loop:true,
+      dots:false,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+    })
+
+    $('.owl-courses').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      dots: false,
+      nav:true,
+      navText: [
+          '<i class="fa fa-angle-left"></i>',
+          '<i class="fa fa-angle-right"></i>'
+      ],
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+    $('.owl-client').owlCarousel({
+      animateOut: 'fadeOut',
+      loop: true,
+      autoplayHoverPause: false,
+      autoplay: true,
+      smartSpeed: 1000,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+        },
+        1000: {
+          items: 3,
+        }
+      }
+    });
+
+
+    // SMOOTHSCROLL
+    $(function() {
+      $('.custom-navbar a, #home a').on('click', function(event) {
+        var $anchor = $(this);
+          $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 49
+          }, 1000);
+            event.preventDefault();
+      });
+    });  
 
 })(jQuery);
